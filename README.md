4631ca3b7abcb34381059850d5b42d568057184e
Vishal Sharma <vishal.sharma2@bp.com>
2022-08-18 11:48:46 +0530

    cypress e2e tests

---
+++ README.md
@@ -0,0 +1,11 @@
+# build-time
+
+## To run e2e tests
+
+- `npm install`
+
+- `npm run e2e`
+
+This will run cypress tests in headless mode
+
+- `npm run e2e:chrome`
---
+++ cypress/e2e/addProducstToCart.cy.js
@@ -0,0 +1,18 @@
+describe('addProductsToCart', () => {
+  it('should add products to cart', () => {
+    cy.visit('http://localhost:3000/products/');
+    cy.get('#go-to-product-details-1').click();
+    cy.get('#add-to-cart').click();
+    cy.get('#cart-count').should('have.text', '1');
+    cy.intercept('https://fakestoreapi.com/products/*').as('getProductDetailsInCart');
+    cy.get('#go-to-cart').click();
+    cy.url().should('contains', 'http://localhost:3000/cart/');
+
+    // cy.getLocal('products').
+    //   then(() => {
+    //     cy.wait('@getProductDetailsInCart').then(() => {
+    //       cy.get('#cart-total').should('have.text', '109.95');
+    //     });
+    //   });
+  });
+});
