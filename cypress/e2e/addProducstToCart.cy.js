ab8f9092f6b6b60e622c90e409e2371293251c7f
Vishal Sharma <vishal.sharma2@bp.com>
2022-08-22 17:13:41 +0530

    adding more steps in e2e tests

--- cypress/e2e/addProducstToCart.cy.js
+++ cypress/e2e/addProducstToCart.cy.js
@@ -1,12 +1,21 @@
 describe('addProductsToCart', () => {
   it('should add products to cart', () => {
     cy.visit('http://localhost:3000/products/');
+
     cy.get('#go-to-product-details-1').click();
     cy.get('#add-to-cart').click();
     cy.get('#cart-count').should('have.text', '1');
-    cy.intercept('https://fakestoreapi.com/products/*').as('getProductDetailsInCart');
+
+    cy.get('#go-back').click();
+
+    cy.get('#go-to-product-details-5').click();
+    cy.get('#add-to-cart').click();
+    cy.get('#cart-count').should('have.text', '2');
+    
     cy.get('#go-to-cart').click();
+
     cy.url().should('contains', 'http://localhost:3000/cart/');
-    cy.get('#cart-total').should('have.text', 'Total: 109.95');
+
+    cy.get('#cart-total').should('have.text', 'Total: 804.95');
   });
 });
